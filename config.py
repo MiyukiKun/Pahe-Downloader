da10982dadd3e1d98ca7010b6a5692bdc480188b
Majethia <hardikmajethia9529@gmail.com>
2023-10-30 22:28:48 +0530

    V1

--- config.py
+++ config.py
@@ -1,18 +1,11 @@
 import os
 import dotenv
 from telethon import TelegramClient
-from pymongo import MongoClient
-from pymongo.collection import Collection
 
 dotenv.load_dotenv(".env")
 
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
-# db_url = os.environ.get('MONGO_DB_URL')
-# database_name = os.environ.get('DATABASE_NAME')
-# owner_id = int(os.environ.get('OWNER_ID'))
-# bot_username = os.environ.get('BOT_USERNAME')
 
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
-# client = MongoClient(db_url, tls=True)
